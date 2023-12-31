63947be684c4adeb175f6321d59eb53750fb01bd
Sascha Kiebler <sascha.kiebler@htwg-konstanz.de>
2023-12-31 13:32:54 +0100

    fixed problem with images wrong encodings

--- pdfAPI.py
+++ pdfAPI.py
@@ -1,39 +1,12 @@
-# {"datei_name":"test_pdf",
-# "datei_typ":"pdf",
-# "autor":"test_autor",
-# "titel":"test_titel",
-# "page_count":2,
-# "topics":["test_topic1","test_topic2"],
-# "pages":[
-#     {"page_num":1,
-#     "text":"test_text",
-#     "pictures":[
-#         {"pic_num":1,
-#         "pic_description":"test_pic_description"}
-#         ],
-#     "topics":["test_topic1","test_topic2"]},
-#     {"page_num":2,
-#     "text":"test_text",
-#     "pictures":[
-#         {"pic_num":1,
-#         "pic_description":"test_pic_description"}
-#         ],
-#     "topics":["test_topic1","test_topic2"]}
-#     ]
-# }
-# needed functions:
-# - extract_text
-# - extract_pictures
-# - extract_metadata
-# - find_topics
-# - describe_pictures
+
 import base64
 import os
-
+import io
 from PyPDF2 import PdfReader
 from PIL import Image
 import json
 import requests
+import fitz
 
 pdfDict = {"datei_name": "test_pdf",
            "datei_typ": "pdf",
@@ -72,35 +45,40 @@ def extract_text(pdf_file_name):
     return text
 
 
+
 def extract_pictures(pdf_file_name):
+    doc = fitz.open(pdf_file_name)
     pictures = {}
-    pictures_as_bytes = {}
-    reader = PdfReader(pdf_file_name)
-    count = 0
-    pagecount = 0
-    title = reader.metadata.get("/Title", pdf_file_name)
-    title = title.split("/")[-1]
-    title = title.split(".")[0]
-    for page in reader.pages:
+    image_count = 0
+    page_count = 0
+    title = os.path.splitext(os.path.basename(pdf_file_name))[0]
+
+    images_dir = "images/" + title
+    if not os.path.exists(images_dir):
+        os.makedirs(images_dir)
+
+    for page in doc:
         pictures_in_page = {}
-        if not os.path.exists("images/" + title):
-            os.mkdir("images/" + title)
+        image_list = page.get_images(full=True)
 
-        for image_file_object in page.images:
-            img_adress = "images/" + title + "/" + str(count) + image_file_object.name
-            with open(img_adress, "wb") as fp:
-                fp.write(image_file_object.data)
+        for img_index, img in enumerate(image_list, start=1):
+            xref = img[0]
+            base_image = doc.extract_image(xref)
+            image_bytes = base_image["image"]
 
-            pictures_as_bytes.update({f"picture_{count}": base64.b64encode(image_file_object.data).decode("utf-8")})
-            pictures_in_page.update({f"picture_{count}": img_adress})
-            count += 1
+            # Use PIL to handle the image
+            img = Image.open(io.BytesIO(image_bytes))
+            img_adress = f"{images_dir}/{image_count}_{img_index}.png"
 
-        pictures.update({f"page_{pagecount}": pictures_in_page})
-        pagecount += 1
+            # Save the image
+            img.save(img_adress, format="PNG")
+            pictures_in_page.update({f"picture_{image_count}": {"path": img_adress, "description": ""}})
+            image_count += 1
 
-    with open("images/" + title + "/" + title + "_pictures_as_bytes" + ".json", "w") as fp:
-        json.dump(pictures_as_bytes, fp)
+        pictures.update({f"page_{page_count}": pictures_in_page})
+        page_count += 1
 
+    doc.close()
     return pictures
 
 
@@ -116,9 +94,32 @@ def find_topics(text):
 
 
 def describe_pictures(pictures):
-    descriptions = {}
 
-    return descriptions
+    for page in pictures:
+        for pic in pictures[page]:
+            # open the pictures as bytes from the path in pictures
+            image = Image.open(pictures[page][pic]["path"])
+
+            # Save the image to a bytes buffer
+            buffer = io.BytesIO()
+            image.save(buffer, format="PNG")
+            buffer.seek(0)
+
+            # Convert to base64
+            img_str = base64.b64encode(buffer.getvalue()).decode()
+
+            description = requests.post(
+                "http://localhost:11434/api/generate",
+                json={
+                    "model": "llava:13b",
+                    "prompt": "Analyze the following picture and describe it in a few sentences. If there is any text in the "
+                              "picture, please also extract it.",
+                    "stream": False,
+                    "images": [img_str]},
+
+            ).json()
+            pictures[page][pic]["description"] = description.get("response", "no description found")
+    return pictures
 
 
 def extract_all_data(pdf_file_name):
@@ -126,10 +127,12 @@ def extract_all_data(pdf_file_name):
     pictures = extract_pictures(pdf_file_name)
     metadata = extract_metadata(pdf_file_name)
     topics = find_topics(text)
-    descriptions = describe_pictures(pictures)
+
     title = metadata.get("/Title", pdf_file_name)
     title = title.split("/")[-1]
     title = title.split(".")[0]
+
+    descripted_pics = describe_pictures(pictures)
 
     data = {}
     data.update({"datei_name": pdf_file_name})
@@ -140,7 +143,7 @@ def extract_all_data(pdf_file_name):
         page_key = f"page_{page_num}"
         data[page_key] = {
             "text": text.get(page_key, ""),
-            "pictures": pictures.get(page_key, {})
+            "pictures": descripted_pics.get(page_key, {})
         }
 
     with open("images/" + title + "/" + title + ".json", "w") as fp:
@@ -148,16 +151,6 @@ def extract_all_data(pdf_file_name):
     return data
 
 
-print(extract_all_data("test_pdfs/41-swen-2-refactoring.pdf"))
+print(extract_all_data("test_pdfs/Kommunikation im Medizinwesen.pdf"))
 
-with open("images/41-swen-2-refactoring/41-swen-2-refactoring_pictures_as_bytes.json", "r") as fp:
-    data = json.load(fp)
-    print(requests.post(
-        "http://localhost:11434/api/generate",
-        json = {
-            "model": "llava:13b",
-            "prompt": "descibe the picture",
-            "stream": False,
-            "images": [data["picture_10"]]},
 
-    ).text)
